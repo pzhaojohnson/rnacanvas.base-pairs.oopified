ec43eade4a2bd85968f2a20a38c21c297c9c8b9c
Philip Johnson <p.zhao.johnson@gmail.com>
2024-03-27 09:23:15 -0400

    Add the ability to access bases in base-pairs by index

--- src/BasePair.test.js
+++ src/BasePair.test.js
@@ -16,4 +16,16 @@ describe('BasePair class', () => {
     let bp = new BasePair(new NucleobaseMock(), secondBase);
     expect(bp.secondBase).toBe(secondBase);
   });
+
+  test('accessing the first base by index', () => {
+    let firstBase = new NucleobaseMock();
+    let bp = new BasePair(firstBase, new NucleobaseMock());
+    expect(bp[0]).toBe(firstBase);
+  });
+
+  test('accessing the second base by index', () => {
+    let secondBase = new NucleobaseMock();
+    let bp = new BasePair(new NucleobaseMock(), secondBase);
+    expect(bp[1]).toBe(secondBase);
+  });
 });
--- src/BasePair.ts
+++ src/BasePair.ts
@@ -3,4 +3,12 @@
  */
 export class BasePair<Nucleobase> {
   constructor(readonly firstBase: Nucleobase, readonly secondBase: Nucleobase) {}
+
+  get '0'(): Nucleobase {
+    return this.firstBase;
+  }
+
+  get '1'(): Nucleobase {
+    return this.secondBase;
+  }
 }
