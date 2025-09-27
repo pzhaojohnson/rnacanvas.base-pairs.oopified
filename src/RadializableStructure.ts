52715d7786244cea6a576577be7b33b7f6961b1d
Philip Johnson <p.zhao.johnson@gmail.com>
2025-09-27 19:30:59 -0400

    Add indexOf() and positionOf() methods to RadializableStructure class

--- src/RadializableStructure.ts
+++ src/RadializableStructure.ts
@@ -7,13 +7,19 @@ export class RadializableStructure<Nucleobase> {
 
   #basePairs;
 
+  #indices = new Map<Nucleobase, number>();
+
   constructor(bases: Nucleobase[], basePairs: BasePairTuple<Nucleobase>[]) {
+    let _: unknown;
+
     // remove pseudoknots, repeat base-pairs, conflicting base-pairs, etc.
-    [bases, basePairs] = radializable(bases, basePairs);
+    [_, basePairs] = radializable(bases, basePairs);
 
     this.#bases = bases;
 
     this.#basePairs = basePairs.map(bp => new BasePair(...bp));
+
+    this.#bases.forEach((b, i) => this.#indices.set(b, i));
   }
 
   get bases(): Iterable<Nucleobase> {
@@ -22,6 +28,32 @@ export class RadializableStructure<Nucleobase> {
 
   get basePairs(): Iterable<BasePair<Nucleobase>> {
     return this.#basePairs.map(bp => bp.deepCopy());
+  }
+
+  /**
+   * Returns the zero-based index of the specified base
+   * in the sequence of bases for the structure.
+   *
+   * Throws if the specified base is not in the structure.
+   */
+  indexOf(b: Nucleobase): number | never {
+    let i = this.#indices.get(b);
+
+    if (i === undefined) {
+      throw new Error('The specified base is not in the structure.');
+    }
+
+    return i;
+  }
+
+  /**
+   * Returns the one-based position of the specified base
+   * in the sequence of bases for the structure.
+   *
+   * Throws if the specified base is not in the structure.
+   */
+  positionOf(b: Nucleobase): number | never {
+    return this.indexOf(b) + 1;
   }
 }
 
